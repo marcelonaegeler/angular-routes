d9d31d3257a1773bb8c7400a052ceff39f9ffec4
Marcelo André Naegeler <marcelo.andre.naegeler@gmail.com>
2014-07-24 08:59:18 -0300

    Page title management :D

--- script.js
+++ script.js
@@ -1,48 +1,60 @@
-	// create the module and name it scotchApp
-	var scotchApp = angular.module('scotchApp', ['ngRoute']);
+// create the module and name it scotchApp
+var scotchApp = angular.module('scotchApp', ['ngRoute']);
 
-	// configure our routes
+// configure our routes
+scotchApp.config(function($routeProvider, $locationProvider) {
+	$routeProvider
+
+		// route for the home page
+		.when('/', {
+			templateUrl: 'pages/home.html',
+			controller: 'mainController',
+			title: ''
+		})
+
+		// route for the about page
+		.when('/about', {
+			templateUrl : 'pages/about.html',
+			controller  : 'aboutController',
+			title: 'About'
+		})
+
+		// route for the contact page
+		.when('/contact', {
+			templateUrl : 'pages/contact.html',
+			controller  : 'contactController',
+			title: 'Get in touch'
+		})
+
+		// route for the contact page
+		.when('/404', {
+			templateUrl : 'pages/404.html',
+			title: 'Page not found'
+		})
+
+		.otherwise({
+			redirectTo: '/404'
+		});
+	$locationProvider.html5Mode(true)
 	
-	
-	scotchApp.config(function($routeProvider, $locationProvider) {
-		$routeProvider
+});
 
-			// route for the home page
-			.when('/', {
-				templateUrl : 'pages/home.html',
-				controller  : 'mainController'
-			})
+scotchApp.run(['$location', '$rootScope', function($location, $rootScope) {
+    $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
+        $rootScope.title = current.$$route.title;
+    });
+}]);
 
-			// route for the about page
-			.when('/about', {
-				templateUrl : 'pages/about.html',
-				controller  : 'aboutController'
-			})
+// create the controller and inject Angular's $scope
+scotchApp.controller('mainController', function($scope) {
+	// create a message to display in our view
+	$scope.message = 'Everyone come and see how good I look!';
+});
 
-			// route for the contact page
-			.when('/contact', {
-				templateUrl : 'pages/contact.html',
-				controller  : 'contactController'
-			});
-		$locationProvider.html5Mode(true)
-		
-	});
-	
-	scotchApp.controller('titleCtrl', function($scope) {
-		$scope.title = 'teste';
-	});
+scotchApp.controller('aboutController', function($scope) {
+	$scope.message = 'Look! I am an about page.';
+});
 
-	// create the controller and inject Angular's $scope
-	scotchApp.controller('mainController', function($scope) {
-		// create a message to display in our view
-		
-		$scope.message = 'Everyone come and see how good I look!';
-	});
-
-	scotchApp.controller('aboutController', function($scope) {
-		$scope.message = 'Look! I am an about page.';
-	});
-
-	scotchApp.controller('contactController', function($scope) {
-		$scope.message = 'Contact us! JK. This is just a demo.';
-	});
+scotchApp.controller('contactController', function($scope) {
+	$scope.message = 'Contact us! JK. This is just a demo.';
+});
